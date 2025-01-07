d225eb21111e0bf0b9267cbf42cf639be5333110
Mina <hominaa99@gmail.com>
2025-01-07 15:10:44 +0330

    jazzmin settings added

--- hotel/settings.py
+++ hotel/settings.py
@@ -130,3 +130,86 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom Admin Settings
+JAZZMIN_SETTINGS = {
+    'site_header': "Mina's Hotel Management",
+    'site_brand': "Your #1 marketplace for collectibles.",
+    'site_logo': "/images/logo.png",
+    'copyright': "All Right Reserved 2023",
+    "welcome_sign": "Welcome to Hotel Management, Login Now.",
+    "topmenu_links": [
+
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"name": "Company", "url": "/admin/addons/company/"},
+        {"name": "Users", "url": "/admin/userauths/user/"},
+
+        {"model": "AUTH_USER_MODEL.User"},
+    ],
+
+    "order_with_respect_to": [
+        "hotel",
+        "hotel.Hotel",
+        "hotel.Room",
+        "hotel.Booking",
+        "hotel.BookingDetail",
+        "hotel.Guest",
+        "hotel.RoomServices",
+        "userauths"
+        "addons",
+    ],
+
+    "icons": {
+        "admin.LogEntry": "fas fa-file",
+
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+
+        "userauths.User": "fas fa-user",
+        "userauths.Profile": "fas fa-address-card",
+
+        "hotel.Hotel": "fas fa-th",
+        "hotel.Booking": "fas fa-calendar-week",
+        "hotel.BookingDetail": "fas fa-calendar-alt",
+        "hotel.Guest": "fas fa-user",
+        "hotel.Room": "fas fa-bed",
+        "hotel.RoomServices": "fas fa-user-cog",
+        "hotel.Notification": "fas fa-bell",
+        "hotel.Coupon": "fas fa-tag",
+        "hotel.Bookmark": "fas fa-heart",
+    },
+
+    "show_ui_builder": True
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": "navbar-indigo",
+    "accent": "accent-olive",
+    "navbar": "navbar-indigo navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-indigo",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cyborg",
+    "dark_mode_theme": "cyborg",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
