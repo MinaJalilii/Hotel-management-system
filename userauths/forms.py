8b9d498452c7be9c287a36c2633c24e884e8fc42
Mina <hominaa99@gmail.com>
2025-01-12 16:09:15 +0330

    register view updated

--- userauths/forms.py
+++ userauths/forms.py
@@ -4,12 +4,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class RegistrationForm(UserCreationForm):
-    full_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'full name'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'username'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'email'}))
-    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'phone number'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'confirm password'}))
+    full_name = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'full name', 'class': 'form-control', 'type': 'text', 'name': 'full_name'}))
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'username', 'class': 'form-control', 'type': 'text', 'name': 'username'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'placeholder': 'email', 'class': 'form-control', 'type': 'email', 'name': 'email'}))
+    phone = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'phone number', 'class': 'form-control', 'type': 'text', 'name': 'phone'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'password', 'class': 'form-control', 'type': 'password', 'name': 'password1'}))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'confirm password', 'class': 'form-control', 'type': 'password',
+                   'name': 'password2'}))
 
     class Meta:
         model = User
--- userauths/views.py
+++ userauths/views.py
@@ -1,25 +1,32 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from userauths.models import User, Profile
-from userauths.forms import UserCreationForm
+from userauths.forms import RegistrationForm
 
 
 def register_user_view(request):
     if request.method == 'POST':
-        if request.user.is_authenticated:
-            return "pass"
-        form = UserCreationForm(request.POST or None)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
+            user = form.save()
             full_name = form.cleaned_data.get('full_name')
-            email = form.cleaned_data.get('email')
-            username = form.cleaned_data.get('username')
             phone = form.cleaned_data.get('phone')
-            password1 = form.cleaned_data.get('password1')
-            password2 = form.cleaned_data.get('password2')
-            user = authenticate(username=username, password=password1)
+
+            profile = Profile.objects.get(user=user)
+            profile.full_name = full_name
+            profile.phone = phone
+            profile.save()
+
+            password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
-        context = {
-            'form': form,
-        }
-        return render(request, 'userauths/register_user.html', context)
+                return redirect('userauths:sign-up')
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'userauths/sign-up.html', context)
